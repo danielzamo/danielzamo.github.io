191c278c5b461762bd4e4451e43b33d79127744d
Daniel Oscar Zamo <daniel.oscar@gmail.com>
2023-06-20 04:01:23 +0200

    rebuilding site Tue Jun 20 04:01:23 AM CEST 2023

---
+++ index.search.js
@@ -0,0 +1,81 @@
+var relearn_search_index = [
+  {
+    "content": "Discover what this Hugo theme is all about and the core-concepts behind it.\n",
+    "description": "",
+    "tags": null,
+    "title": "Basics",
+    "uri": "/basics/index.html"
+  },
+  {
+    "content": "En esta sección comparto artículos de Kubernetes.\n",
+    "description": "",
+    "tags": null,
+    "title": "Kubernetes",
+    "uri": "/kubernetes/index.html"
+  },
+  {
+    "content": "En esta sección se muestra como definir un daemonset.\nEste tipo de componentes de Kubernetes se ejecutaran sobre todos los nodos del cluster. Este tipo puede utilizarse por ejemplo para monitorizar los nodos (si están disponibles).\n",
+    "description": "",
+    "tags": [
+      "documentation",
+      "k8s"
+    ],
+    "title": "Daemonset",
+    "uri": "/kubernetes/03-daemonset/index.html"
+  },
+  {
+    "content": "En esta sección se muestra como definir un deployment.\nEste tipo de componentes de Kubernetes se ejecutaran sobre todos los nodos del cluster. Este tipo puede utilizarse por ejemplo para monitorizar los nodos (si están disponibles).\n",
+    "description": "",
+    "tags": [
+      "documentation",
+      "k8s"
+    ],
+    "title": "Deployment",
+    "uri": "/kubernetes/04-deployment/index.html"
+  },
+  {
+    "content": "En esta sección se muestra como definir un services.\nEste tipo de componentes de Kubernetes se ejecutaran sobre todos los nodos del cluster. Este tipo puede utilizarse por ejemplo para monitorizar los nodos (si están disponibles).\n",
+    "description": "",
+    "tags": [
+      "documentation",
+      "k8s"
+    ],
+    "title": "Services",
+    "uri": "/kubernetes/05-services/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tag :: documentation",
+    "uri": "/tags/documentation/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tag :: k8s",
+    "uri": "/tags/k8s/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  },
+  {
+    "content": "En este sitio comparto algunos artículos e implementaciones que utilizo como referencias de estudio o lectura.\nEl sitio esta generado con Hugo y utiliza el theme Hugo Relearn Theme.\n",
+    "description": "",
+    "tags": null,
+    "title": "Web danielzamo",
+    "uri": "/index.html"
+  }
+]
