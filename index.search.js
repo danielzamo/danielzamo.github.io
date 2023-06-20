0e54f67afc6f6f93ee69f1fe9d017b3cf25baa6a
Daniel Oscar Zamo <daniel.oscar@gmail.com>
2023-06-20 03:13:56 +0200

    rebuilding site Tue Jun 20 03:13:56 AM CEST 2023

---
+++ index.search.js
@@ -0,0 +1,37 @@
+var relearn_search_index = [
+  {
+    "content": "Discover what this Hugo theme is all about and the core-concepts behind it.\n",
+    "description": "",
+    "tags": null,
+    "title": "Basics",
+    "uri": "/basics/index.html"
+  },
+  {
+    "content": "En esta sección comparto artículos de Kubernetes.\n",
+    "description": "",
+    "tags": null,
+    "title": "Kubernetes",
+    "uri": "/kubernetes/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Site",
+    "uri": "/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
