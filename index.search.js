65661369a0860214ba2508c2942f64c3d8bae8a8
Daniel Oscar Zamo <daniel.oscar@gmail.com>
2023-07-06 03:17:09 +0200

    rebuilding site Thu Jul  6 03:17:09 AM CEST 2023

--- index.search.js
+++ index.search.js
@@ -31,7 +31,7 @@ var relearn_search_index = [
     "uri": "/kubernetes-notes/index.html"
   },
   {
-    "content": " En esta sección se comparten configuraciones de servicios y/o configuración de servidores basados en Ubuntu server LTS.\nURL repositorio: PENDIENTE. Por el momento hasta que finalize el proyecto, el contanido solo es compartido aquí, en este sitio web estático\nTecnologías usadas Distribución Linux principal usada: Ubuntu server LTS - 22.04.. Despliegues/artículos actuales compartidos aquí Anfitrión KVMUbuntu server LTS, host anfitrión virtualización KVM\n",
+    "content": " En esta sección se comparten configuraciones de servicios y/o configuración de servidores basados en Ubuntu server LTS.\nURL repositorio: PENDIENTE. Por el momento hasta que finalize el proyecto, el contanido solo es compartido aquí, en este sitio web estático\nTecnologías usadas Distribución Linux principal usada: Ubuntu server LTS - 22.04.. Despliegues/artículos actuales compartidos aquí Anfitrión KVMUbuntu server LTS, host anfitrión virtualización KVM\nMinikube driver kvmUbuntu server LTS, cluster k8s minikube (driver kvm)\n",
     "description": "Ubuntu server LTS",
     "tags": null,
     "title": "Ubuntu server LTS",
@@ -68,6 +68,21 @@ var relearn_search_index = [
     ],
     "title": "Definición pod mediante manifiesto",
     "uri": "/kubernetes-notes/pod/pod-minimal/index.html"
+  },
+  {
+    "content": " Despliegue de servidor host anfitrión host virtualización KVM. Despliegue de cluster Ubuntu server LTS k8s minikube (driver kvm).\nTecnologías utilizadas minikube KVM Ubuntu LTS 22.04.. Especificación inicial …\nDependecias cumplida … … Actualización sistema, instalando paquetes (opcionales) dzamo@ubuntu-k8s:~$ sudo apt -y update \u0026\u0026 sudo apt -y upgrade Instalación/configuración de minikube En este apartado se instala y configura un cluster minikube con driver kvm (REF LINK).\n# Instalacion minikube wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 sudo install minikube-linux-amd64 /usr/local/bin/minikube minikube version Instalación de kubectl sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl \\ -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl sudo chmod 755 ./kubectl sudo mv kubectl /usr/local/bin/ kubectl version -o json --client ",
+    "description": "Ubuntu server LTS, cluster k8s minikube (driver kvm)",
+    "tags": [
+      "kubernetes",
+      "projects",
+      "KVM",
+      "UbuntuLTS",
+      "minikube",
+      "container",
+      "virtualization"
+    ],
+    "title": "Minikube driver kvm",
+    "uri": "/ubuntu-lts-server/minikube-kvm/index.html"
   },
   {
     "content": "Relevantes en esta nota Sección variables de entorno, definición env en manifiesto. Utilización de variables recuperadas desde kubernetes Downward API Manifiesto apiVersion: v1 kind: Pod metadata: name: nginx spec: containers: - name: nginx image: nginx:alpine env: - name: var_1 value: \"value_val_1\" - name: var_2 value: \"value_val_2\" - name: DD_AGENT_HOST valueFrom: fieldRef: fieldPath: status.hostIP env En la sección env se pueden definir/pasar variables de entornos. En lo anterior es:\nenv: # Variables de entorno - name: var_1 ## 'Nombre 1' value: \"value_val_1\" ## Valor variable 'Nombre 1' - name: var_2 ## 'Nombre 2' value: \"value_val_2\" ## Valor variable 'Nombre 2' - name: dd_agent_host ## Variable definida desde el \"Downward API\" (son variables/valores que se pueden consultar) valueFrom: fieldRef: fieldPath: status.hostIP ## Se asigna el valor de hostIP (su valor se recupera desde la \"Downward API\" de kubernetes) donde esta corriendo el pod, en este caso. Sesión comandos [dzamo@victus kubernetes]$ kubectl apply -f 03-pod-w-env.yaml [dzamo@victus kubernetes]$ kubectl get pod NAME READY STATUS RESTARTS AGE nginx-w-env 1/1 Running 0 18s [dzamo@victus kubernetes]$ kubectl exec nginx-w-env -- env PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin HOSTNAME=nginx-w-env NGINX_VERSION=1.25.1 PKG_RELEASE=1 NJS_VERSION=0.7.12 dd_agent_host=10.0.0.84 var_1=value_var_1 var_2=value_var_2 KUBERNETES_PORT=tcp://10.43.0.1:443 KUBERNETES_PORT_443_TCP=tcp://10.43.0.1:443 KUBERNETES_PORT_443_TCP_PROTO=tcp KUBERNETES_PORT_443_TCP_PORT=443 KUBERNETES_PORT_443_TCP_ADDR=10.43.0.1 KUBERNETES_SERVICE_HOST=10.43.0.1 KUBERNETES_SERVICE_PORT=443 KUBERNETES_SERVICE_PORT_HTTPS=443 HOME=/root [dzamo@victus kubernetes]$ kubectl exec -it nginx-w-env -- sh / # hostname nginx-w-env / # exit ",
@@ -110,6 +125,13 @@ var relearn_search_index = [
     "content": "",
     "description": "",
     "tags": null,
+    "title": "Tag :: container",
+    "uri": "/tags/container/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
     "title": "Tag :: documentation",
     "uri": "/tags/documentation/index.html"
   },
@@ -133,6 +155,13 @@ var relearn_search_index = [
     "tags": null,
     "title": "Tag :: KVM",
     "uri": "/tags/kvm/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tag :: minikube",
+    "uri": "/tags/minikube/index.html"
   },
   {
     "content": "",
