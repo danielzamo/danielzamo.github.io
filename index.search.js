39330e376711558af9f1ed4376c8ae03a5e2989f
Daniel Oscar Zamo <daniel.oscar@gmail.com>
2023-07-06 02:20:15 +0200

    rebuilding site Thu Jul  6 02:20:15 AM CEST 2023

--- index.search.js
+++ index.search.js
@@ -31,11 +31,23 @@ var relearn_search_index = [
     "uri": "/kubernetes-notes/index.html"
   },
   {
-    "content": " En esta sección se comparten configuraciones de servicios y/o configuración de servidores basados en Ubuntu server LTS.\nURL repositorio: PENDIENTE. Por el momento hasta que finalize el proyecto, el contanido solo es compartido aquí, en este sitio web estático\nTecnologías usadas Distribución Linux principal usada: Ubuntu server LTS - 22.04.. Contenido actual despliegues/artículos compartidos Ubuntu LTS - KVMUbuntu server LTS, host anfitrión virtualización KVM\n",
+    "content": " En esta sección se comparten configuraciones de servicios y/o configuración de servidores basados en Ubuntu server LTS.\nURL repositorio: PENDIENTE. Por el momento hasta que finalize el proyecto, el contanido solo es compartido aquí, en este sitio web estático\nTecnologías usadas Distribución Linux principal usada: Ubuntu server LTS - 22.04.. Despliegues/artículos actuales compartidos aquí Anfitrión KVMUbuntu server LTS, host anfitrión virtualización KVM\n",
     "description": "Ubuntu server LTS",
     "tags": null,
     "title": "Ubuntu server LTS",
     "uri": "/ubuntu-lts-server/index.html"
+  },
+  {
+    "content": " Despliegue de servidor host anfitrión host virtualización KVM.\nEspecificación inicial 1 x bridge 1 x ethernet IP\nIP gateway network DNS 192.168.123.35 192.168.123.1 192.168.123.0/24 192.168.123.1,8.8.8.8,1.1.1.1 Instalación/configuración de KVM # Install KVM in Ubuntu dzamo@ubuntu-k8s:~$ sudo apt update \u0026\u0026 sudo apt upgrade dzamo@ubuntu-k8s:~$ sudo apt -y install qemu-kvm libvirt-dev bridge-utils libvirt-daemon-system libvirt-daemon virtinst bridge-utils libosinfo-bin libguestfs-tools virt-top dzamo@ubuntu-k8s:~$ ls dzamo@ubuntu-k8s:~$ sudo usermod -a $USER -G libvirt,libvirt-qemu,libvirt-dnsmasq,kvm dzamo@ubuntu-k8s:~$ sudo modprobe vhost_net dzamo@ubuntu-k8s:~$ sudo lsmod | grep vhost dzamo@ubuntu-k8s:~$ echo \"vhost_net\" | sudo tee -a /etc/modules dzamo@ubuntu-k8s:~$ sudo vim /etc/netplan/00-installer-config.yaml dzamo@ubuntu-k8s:~$ sudo apt install neovim tmux vim vim-scripts exuberant-ctags python-greenlet-dev dzamo@ubuntu-k8s:~$ cat /etc/netplan/00-installer-config.yaml network: ethernets: enp1s0: dhcp4: false dhcp6: false bridges: br0: interfaces: [enp1s0] dhcp4: false addresses: [192.168.123.35/24] macaddress: 52:54:00:e8:a8:08 routes: - to: default via: 192.168.123.1 metric: 100 nameservers: addresses: [192.168.123.1,1.1.1.1,8.8.8.8] search: [my.net] parameters: stp: false dhcp6: false version: 2 dzamo@ubuntu-k8s:~$ sudo netplan apply dzamo@ubuntu-k8s:~$ ip a dzamo@ubuntu-k8s:~$ wget www.google.com dzamo@ubuntu-k8s:~$ rm index.html dzamo@ubuntu-k8s:~$ ip a dzamo@ubuntu-k8s:~$ sudo reboot ",
+    "description": "Ubuntu server LTS, host anfitrión virtualización KVM",
+    "tags": [
+      "virtualization",
+      "projects",
+      "KVM",
+      "UbuntuLTS"
+    ],
+    "title": "Anfitrión KVM",
+    "uri": "/ubuntu-lts-server/kvm/index.html"
   },
   {
     "content": "Este proyecto es el despliegue de Ansible AWX, edición comunitaria de Red Hat Ansible Tower.\nEn la siguiente representación se muestra un diagrama del despliegue. El cual es realizado sobre un host motor de kubernetes con minikube con libvirt como runtime (driver kvm).\nTecnologías utilizadas Contenedores: kubernetes / k3s or minikube Dist. Linux: AlmaLinux 9 Sesión de instalación/configuración La sesión siguiente fue realizada tanto sobre un cluster de minikube como en uno con k3s. Aquí se comparte el despligue realizado con k3s.\n# Ansible AWX in kubernetes k3s git clone https://github.com/ansible/awx-operator.git cd awx-operator/ git checkout 2.2.1 git branch export NAMESPACE=ansible-awx make deploy kubectl get pods -n $NAMESPACE cp awx-demo.yml ansible-awx.yml # Modifico el nombre del namespace a crear sed -i 's/name: awx-demo/name: ansible-awx/g' ansible-awx.yml sudo /usr/local/bin/kubectl config set-context --current --namespace=$NAMESPACE kubectl apply -f ansible-awx.yml # Reviso el estado del despliegue, para luego continuar kubectl logs -f deployments/awx-operator-controller-manager -c awx-manager kubectl get pods -l \"app.kubernetes.io/managed-by=awx-operator\" kubectl get service -l \"app.kubernetes.io/managed-by=awx-operator\" # Genero una primera contraseña para el usuario admin pueda ingresar a la interface web kubectl get secret ansible-awx-admin-password -o jsonpath=\"{.data.password}\" | base64 --decode; echo # Forward al servicio para acceder externamente kubectl port-forward service/ansible-awx-service --address 0.0.0.0 10445:80 \u0026 Ansible AWX debería estar disponible en el URL http://\u003cIP_SERVIDOR_KUBERNETES\u003e:10445. En este caso ahora implementado el ingreso a la interface Web se muestra en la siguiente captura.\nRepositorio …\n",
@@ -56,18 +68,6 @@ var relearn_search_index = [
     ],
     "title": "Definición pod mediante manifiesto",
     "uri": "/kubernetes-notes/pod/pod-minimal/index.html"
-  },
-  {
-    "content": " Despliegue de servidor host anfitrión host virtualización KVM.\nEspecificación inicial 1 x bridge 1 x ethernet IP\nIP gateway network DNS 192.168.123.35 192.168.123.1 192.168.123.0/24 192.168.123.1,8.8.8.8,1.1.1.1 Instalación/configuración de KVM # Install KVM in Ubuntu dzamo@ubuntu-k8s:~$ sudo apt update \u0026\u0026 sudo apt upgrade dzamo@ubuntu-k8s:~$ sudo apt -y install qemu-kvm libvirt-dev bridge-utils libvirt-daemon-system libvirt-daemon virtinst bridge-utils libosinfo-bin libguestfs-tools virt-top dzamo@ubuntu-k8s:~$ ls dzamo@ubuntu-k8s:~$ sudo usermod -a $USER -G libvirt,libvirt-qemu,libvirt-dnsmasq,kvm dzamo@ubuntu-k8s:~$ sudo modprobe vhost_net dzamo@ubuntu-k8s:~$ sudo lsmod | grep vhost dzamo@ubuntu-k8s:~$ echo \"vhost_net\" | sudo tee -a /etc/modules dzamo@ubuntu-k8s:~$ sudo vim /etc/netplan/00-installer-config.yaml dzamo@ubuntu-k8s:~$ sudo apt install neovim tmux vim vim-scripts exuberant-ctags python-greenlet-dev dzamo@ubuntu-k8s:~$ cat /etc/netplan/00-installer-config.yaml network: ethernets: enp1s0: dhcp4: false dhcp6: false bridges: br0: interfaces: [enp1s0] dhcp4: false addresses: [192.168.123.35/24] macaddress: 52:54:00:e8:a8:08 routes: - to: default via: 192.168.123.1 metric: 100 nameservers: addresses: [192.168.123.1,1.1.1.1,8.8.8.8] search: [my.net] parameters: stp: false dhcp6: false version: 2 dzamo@ubuntu-k8s:~$ sudo netplan apply dzamo@ubuntu-k8s:~$ ip a dzamo@ubuntu-k8s:~$ wget www.google.com dzamo@ubuntu-k8s:~$ rm index.html dzamo@ubuntu-k8s:~$ ip a dzamo@ubuntu-k8s:~$ sudo reboot ",
-    "description": "Ubuntu server LTS, host anfitrión virtualización KVM",
-    "tags": [
-      "virtualization",
-      "projects",
-      "KVM",
-      "UbuntuLTS"
-    ],
-    "title": "Ubuntu LTS - KVM",
-    "uri": "/ubuntu-lts-server/kvm/index.html"
   },
   {
     "content": "Relevantes en esta nota Sección variables de entorno, definición env en manifiesto. Utilización de variables recuperadas desde kubernetes Downward API Manifiesto apiVersion: v1 kind: Pod metadata: name: nginx spec: containers: - name: nginx image: nginx:alpine env: - name: var_1 value: \"value_val_1\" - name: var_2 value: \"value_val_2\" - name: DD_AGENT_HOST valueFrom: fieldRef: fieldPath: status.hostIP env En la sección env se pueden definir/pasar variables de entornos. En lo anterior es:\nenv: # Variables de entorno - name: var_1 ## 'Nombre 1' value: \"value_val_1\" ## Valor variable 'Nombre 1' - name: var_2 ## 'Nombre 2' value: \"value_val_2\" ## Valor variable 'Nombre 2' - name: dd_agent_host ## Variable definida desde el \"Downward API\" (son variables/valores que se pueden consultar) valueFrom: fieldRef: fieldPath: status.hostIP ## Se asigna el valor de hostIP (su valor se recupera desde la \"Downward API\" de kubernetes) donde esta corriendo el pod, en este caso. Sesión comandos [dzamo@victus kubernetes]$ kubectl apply -f 03-pod-w-env.yaml [dzamo@victus kubernetes]$ kubectl get pod NAME READY STATUS RESTARTS AGE nginx-w-env 1/1 Running 0 18s [dzamo@victus kubernetes]$ kubectl exec nginx-w-env -- env PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin HOSTNAME=nginx-w-env NGINX_VERSION=1.25.1 PKG_RELEASE=1 NJS_VERSION=0.7.12 dd_agent_host=10.0.0.84 var_1=value_var_1 var_2=value_var_2 KUBERNETES_PORT=tcp://10.43.0.1:443 KUBERNETES_PORT_443_TCP=tcp://10.43.0.1:443 KUBERNETES_PORT_443_TCP_PROTO=tcp KUBERNETES_PORT_443_TCP_PORT=443 KUBERNETES_PORT_443_TCP_ADDR=10.43.0.1 KUBERNETES_SERVICE_HOST=10.43.0.1 KUBERNETES_SERVICE_PORT=443 KUBERNETES_SERVICE_PORT_HTTPS=443 HOME=/root [dzamo@victus kubernetes]$ kubectl exec -it nginx-w-env -- sh / # hostname nginx-w-env / # exit ",
