ea2d3b29b2e5956eb5de8964c22a32d7ae722c83
Daniel Oscar Zamo <daniel.oscar@gmail.com>
2023-07-28 23:39:42 +0200

    rebuilding site Fri Jul 28 11:39:42 PM CEST 2023

--- index.search.js
+++ index.search.js
@@ -106,6 +106,13 @@ var relearn_search_index = [
     "uri": "/kubernetes-notes/pod/pod-minimal/index.html"
   },
   {
+    "content": " En esta sección se comparten artículos de KVM (Kernel Virtual Machine), virtualización completa para Linux en hardware x86.\nPor el momento, en esta sección se publican los siguientes casos de uso (despliegues/implementaciones):\nComando virshAdministración de máquinas virtuales invitadas KVM/libvirt\n",
+    "description": "Kernel Virtual Machine - KVM",
+    "tags": null,
+    "title": "KVM",
+    "uri": "/kvm/index.html"
+  },
+  {
     "content": " Despliegue de cluster Ubuntu server LTS k8s minikube (driver kvm), en un simple host.\nTecnologías utilizadas Minikube KVM Ubuntu LTS 22.04.. Especificación inicial …\nDependecias cumplida En este depliegue se implementa un cluster minikube sobre un simple host. El driver usado es kvm. Se da por supuesto que se ha instalado el host anfitrión KVM. Si necesita ayuda puede consultar instalar anfitrión KVM en Ubuntu LTS, en este mismo sitio.\nActualización sistema, instalando paquetes (opcionales) dzamo@ubuntu-k8s:~$ sudo apt -y update \u0026\u0026 sudo apt -y upgrade Instalación/configuración de minikube En este apartado se instala y configura un cluster minikube con driver kvm (REF LINK).\ndzamo@ubuntu-k8s:~$ # Instalacion minikube dzamo@ubuntu-k8s:~$ wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 dzamo@ubuntu-k8s:~$ sudo install minikube-linux-amd64 /usr/local/bin/minikube dzamo@ubuntu-k8s:~$ minikube version Instalación de kubectl dzamo@ubuntu-k8s:~$ sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl \\ -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl dzamo@ubuntu-k8s:~$ sudo chmod 755 ./kubectl dzamo@ubuntu-k8s:~$ sudo mv kubectl /usr/local/bin/ dzamo@ubuntu-k8s:~$ kubectl version -o json --client ",
     "description": "Ubuntu server LTS, cluster k8s minikube (driver kvm)",
     "tags": [
@@ -162,13 +169,6 @@ var relearn_search_index = [
     "uri": "/kubernetes-notes/pod/daemonset/index.html"
   },
   {
-    "content": " En esta sección se comparten artículos de KVM (Kernel Virtual Machine), virtualización completa para Linux en hardware x86.\nPor el momento, en esta sección se publican los siguientes casos de uso (despliegues/implementaciones):\nComando virshAdministración de máquinas virtuales invitadas KVM/libvirt\n",
-    "description": "Kernel Virtual Machine - KVM",
-    "tags": null,
-    "title": "KVM",
-    "uri": "/kvm/index.html"
-  },
-  {
     "content": "",
     "description": "",
     "tags": null,
@@ -211,7 +211,7 @@ var relearn_search_index = [
     "uri": "/tags/fedora/index.html"
   },
   {
-    "content": "En este sitio comparto algunos artículos, implementaciones y/o proyectos implementados.\nEste sitio esta generado con Hugo y utiliza inicialmente el theme Hugo Relearn Theme.\nAlgunos de los artículos y/o proyectos compartidos aquí son:\nKubernetes cluster k3sCluster kubernetes con k3s. 1 nodo master (control-plane) + 'N' nodos worker\nKubernetes - desplieguesAplicaciones y/o servicios de Kubernetes\nUbuntuUbuntu server LTS\nKubernetes - notasEntradas relacionadas a Kubernetes. Todo el código implementado y los casos de uso probados se encuentran disponible en el repositorio my-code del sitio de Gitlab.\nFedora 38 - PandocInstalación y configuración de Pandoc sobre Fedora 38\nKVMKernel Virtual Machine - KVM\n",
+    "content": "En este sitio comparto algunos artículos, implementaciones y/o proyectos implementados.\nEste sitio esta generado con Hugo y utiliza inicialmente el theme Hugo Relearn Theme.\nAlgunos de los artículos y/o proyectos compartidos aquí son:\nKubernetes cluster k3sCluster kubernetes con k3s. 1 nodo master (control-plane) + 'N' nodos worker\nKubernetes - desplieguesAplicaciones y/o servicios de Kubernetes\nUbuntuUbuntu server LTS\nKubernetes - notasEntradas relacionadas a Kubernetes. Todo el código implementado y los casos de uso probados se encuentran disponible en el repositorio my-code del sitio de Gitlab.\nKVMKernel Virtual Machine - KVM\nFedora 38 - PandocInstalación y configuración de Pandoc sobre Fedora 38\n",
     "description": "",
     "tags": null,
     "title": "Inicio sitio",
