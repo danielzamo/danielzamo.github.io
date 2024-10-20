9818e1efca17c80029ddd510b186a5cf46ca20e7
Daniel Oscar Zamo <daniel.oscar.zamo@gmail.com>
2024-10-20 02:41:38 +0200

    update Sun Oct 20 02:41:38 AM CEST 2024

--- searchindex.js
+++ searchindex.js
@@ -1,26 +1,126 @@
 var relearn_searchindex = [
   {
-    "breadcrumb": "",
-    "content": "This is a new chapter.",
-    "description": "This is a new chapter.",
-    "tags": [],
-    "title": "First Chapter",
-    "uri": "/first-chapter/index.html"
+    "breadcrumb": "Inicio",
+    "content": "Jekyll sobre Linux\nJekyll en Fedora 40 (desktop KDE Plasma 6.2)",
+    "description": "Jekyll sobre Linux\nJekyll en Fedora 40 (desktop KDE Plasma 6.2)",
+    "tags": [
+      "Programación",
+      "Generadores Sitios Estáticos",
+      "Jekyll",
+      "Fedora",
+      "Linux"
+    ],
+    "title": "Jekyll sobre Linux",
+    "uri": "/jekyll-on-linux/index.html"
   },
   {
-    "breadcrumb": "",
+    "breadcrumb": "Inicio \u003e Jekyll sobre Linux",
+    "content": "Instalación de pre requisitos sudo dnf -y install ruby ruby-devel openssl-devel \\ redhat-rpm-config gcc-c++ @development-tools Verificaciones ruby -v; gcc -v; g++ -v; make -v Instalando RubyGems Evitar instalar los paquetes de RubyGems (llamados gemas) como usuario root. En su lugar, configurar un directorio de instalación de gemas para una cuenta de usuario. Los siguientes comandos agregarán variables de entorno al ~/.bashrc para configurar la ruta de instalación de gemas.\necho '# Install Ruby Gems to ~/.config/gems' \u003e\u003e ~/.bashrc echo 'export GEM_HOME=\"$HOME/.config/gems\"' \u003e\u003e ~/.bashrc echo 'export PATH=\"$HOME/.config/gems/bin:$PATH\"' \u003e\u003e ~/.bashrc source ~/.bashrc Instalar Jekyll y Bundler gem install jekyll bundler Otros comandos Instalar/update RubyGems gem update --system",
+    "description": "Instalación de pre requisitos sudo dnf -y install ruby ruby-devel openssl-devel \\ redhat-rpm-config gcc-c++ @development-tools Verificaciones ruby -v; gcc -v; g++ -v; make -v Instalando RubyGems Evitar instalar los paquetes de RubyGems (llamados gemas) como usuario root. En su lugar, configurar un directorio de instalación de gemas para una cuenta de usuario. Los siguientes comandos agregarán variables de entorno al ~/.bashrc para configurar la ruta de instalación de gemas.",
+    "tags": [
+      "Programación",
+      "Generadores Sitios Estáticos",
+      "Jekyll",
+      "Fedora",
+      "Linux"
+    ],
+    "title": "Jekyll en Fedora 40",
+    "uri": "/jekyll-on-linux/jekyll-on-fedora40/index.html"
+  },
+  {
+    "breadcrumb": "Inicio \u003e Jekyll sobre Linux",
+    "content": "Inicio rápido Una vez instalado el generador de sitios Jenkyll se puede crear un sitio web estático. Luego este se podra modificar tanto la apariencia, las URL, los datos que se muestran en la página y más.\nLa instalación del generador de sitios estático (SSG) y sus dependencias se pueden consultar en Jekyll - quickstart y/o en este mismo sitio.\nCrear un nuevo sitio Jekyll jekyll new mysite cd mysite Contruir el sitio y desplegarlo en local bundle exec jekyll serve Si todo a ido bien el sitio debería estar disponible en el URL http://localhost:4000.\nEn la captura se muestra el sitio inicial que genera Jekyll.",
+    "description": "Inicio rápido Una vez instalado el generador de sitios Jenkyll se puede crear un sitio web estático. Luego este se podra modificar tanto la apariencia, las URL, los datos que se muestran en la página y más.\nLa instalación del generador de sitios estático (SSG) y sus dependencias se pueden consultar en Jekyll - quickstart y/o en este mismo sitio.",
+    "tags": [
+      "Programación",
+      "Generadores Sitios Estáticos",
+      "Jekyll"
+    ],
+    "title": "Jekyll - guía de inicio rápida",
+    "uri": "/jekyll-on-linux/quickstart/index.html"
+  },
+  {
+    "breadcrumb": "Inicio \u003e Jekyll sobre Linux",
+    "content": "En esta entrada se indican algunos de los URL y/o sitios consultados para realizar este trabajo compartido.\nReferencias Installing Ruby. Download RubyGems. Jekyll - Quickstart.",
+    "description": "En esta entrada se indican algunos de los URL y/o sitios consultados para realizar este trabajo compartido.\nReferencias Installing Ruby. Download RubyGems. Jekyll - Quickstart.",
+    "tags": [
+      "Programación",
+      "Generadores Sitios Estáticos",
+      "Jekyll",
+      "Fedora",
+      "Linux"
+    ],
+    "title": "Jekyll - referencias consultadas",
+    "uri": "/jekyll-on-linux/jekyll-references/index.html"
+  },
+  {
+    "breadcrumb": "Inicio \u003e Tags",
     "content": "",
     "description": "",
     "tags": [],
-    "title": "Categories",
-    "uri": "/categories/index.html"
+    "title": "Tag :: Fedora",
+    "uri": "/tags/fedora/index.html"
+  },
+  {
+    "breadcrumb": "Inicio \u003e Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Generadores Sitios Estáticos",
+    "uri": "/tags/generadores-sitios-est%C3%A1ticos/index.html"
   },
   {
     "breadcrumb": "",
+    "content": "Artículos tipo capítulos Jekyll sobre Linux",
+    "description": "Artículos tipo capítulos Jekyll sobre Linux",
+    "tags": [
+      "Programación",
+      "Generadores Sitios Estáticos",
+      "Jekyll",
+      "Fedora",
+      "Linux"
+    ],
+    "title": "Inicio",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "Inicio \u003e Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Jekyll",
+    "uri": "/tags/jekyll/index.html"
+  },
+  {
+    "breadcrumb": "Inicio \u003e Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Linux",
+    "uri": "/tags/linux/index.html"
+  },
+  {
+    "breadcrumb": "Inicio \u003e Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Programación",
+    "uri": "/tags/programaci%C3%B3n/index.html"
+  },
+  {
+    "breadcrumb": "Inicio",
     "content": "",
     "description": "",
     "tags": [],
     "title": "Tags",
     "uri": "/tags/index.html"
+  },
+  {
+    "breadcrumb": "Inicio",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
   }
 ]
