02651745b510cdfd3eb6d330b4d9ffd09f1b2a63
Daniel Oscar Zamo <daniel.oscar.zamo@gmail.com>
2024-10-20 22:36:39 +0200

    update Sun Oct 20 10:36:39 PM CEST 2024

--- searchindex.js
+++ searchindex.js
@@ -1,8 +1,8 @@
 var relearn_searchindex = [
   {
-    "breadcrumb": "Inicio",
-    "content": "Jekyll sobre Linux\nJekyll en Fedora 40 (desktop KDE Plasma 6.2)",
-    "description": "Jekyll sobre Linux\nJekyll en Fedora 40 (desktop KDE Plasma 6.2)",
+    "breadcrumb": "",
+    "content": "Jekyll en Linux",
+    "description": "Jekyll en Linux",
     "tags": [
       "Programación",
       "Generadores Sitios Estáticos",
@@ -14,7 +14,7 @@ var relearn_searchindex = [
     "uri": "/jekyll-on-linux/index.html"
   },
   {
-    "breadcrumb": "Inicio \u003e Jekyll sobre Linux",
+    "breadcrumb": "Jekyll sobre Linux",
     "content": "Instalación de pre requisitos sudo dnf -y install ruby ruby-devel openssl-devel \\ redhat-rpm-config gcc-c++ @development-tools Verificaciones ruby -v; gcc -v; g++ -v; make -v Instalando RubyGems Evitar instalar los paquetes de RubyGems (llamados gemas) como usuario root. En su lugar, configurar un directorio de instalación de gemas para una cuenta de usuario. Los siguientes comandos agregarán variables de entorno al ~/.bashrc para configurar la ruta de instalación de gemas.\necho '# Install Ruby Gems to ~/.config/gems' \u003e\u003e ~/.bashrc echo 'export GEM_HOME=\"$HOME/.config/gems\"' \u003e\u003e ~/.bashrc echo 'export PATH=\"$HOME/.config/gems/bin:$PATH\"' \u003e\u003e ~/.bashrc source ~/.bashrc Instalar Jekyll y Bundler gem install jekyll bundler Otros comandos Instalar/update RubyGems gem update --system",
     "description": "Instalación de pre requisitos sudo dnf -y install ruby ruby-devel openssl-devel \\ redhat-rpm-config gcc-c++ @development-tools Verificaciones ruby -v; gcc -v; g++ -v; make -v Instalando RubyGems Evitar instalar los paquetes de RubyGems (llamados gemas) como usuario root. En su lugar, configurar un directorio de instalación de gemas para una cuenta de usuario. Los siguientes comandos agregarán variables de entorno al ~/.bashrc para configurar la ruta de instalación de gemas.",
     "tags": [
@@ -28,9 +28,9 @@ var relearn_searchindex = [
     "uri": "/jekyll-on-linux/jekyll-on-fedora40/index.html"
   },
   {
-    "breadcrumb": "Inicio \u003e Jekyll sobre Linux",
+    "breadcrumb": "Jekyll sobre Linux",
     "content": "Inicio rápido Una vez instalado el generador de sitios Jenkyll se puede crear un sitio web estático. Luego este se podra modificar tanto la apariencia, las URL, los datos que se muestran en la página y más.\nLa instalación del generador de sitios estático (SSG) y sus dependencias se pueden consultar en Jekyll y/o en este mismo sitio.\nCrear un nuevo sitio Jekyll jekyll new mysite cd mysite Contruir el sitio y desplegarlo en local bundle exec jekyll serve Si todo a ido bien el sitio debería estar disponible en el URL http://localhost:4000.\nEn la captura se muestra el sitio inicial que genera Jekyll.",
-    "description": "Inicio rápido Una vez instalado el generador de sitios Jenkyll se puede crear un sitio web estático. Luego este se podra modificar tanto la apariencia, las URL, los datos que se muestran en la página y más.\nLa instalación del generador de sitios estático (SSG) y sus dependencias se pueden consultar en Jekyll y/o en este mismo sitio.",
+    "description": "",
     "tags": [
       "Programación",
       "Generadores Sitios Estáticos",
@@ -40,9 +40,20 @@ var relearn_searchindex = [
     "uri": "/jekyll-on-linux/quickstart/index.html"
   },
   {
-    "breadcrumb": "Inicio \u003e Jekyll sobre Linux",
+    "breadcrumb": "Jekyll sobre Linux",
     "content": "En esta entrada se indican algunos de los URL y/o sitios consultados para realizar este trabajo compartido.\nReferencias Installing Ruby. Download RubyGems. Jekyll - Quickstart.",
-    "description": "En esta entrada se indican algunos de los URL y/o sitios consultados para realizar este trabajo compartido.\nReferencias Installing Ruby. Download RubyGems. Jekyll - Quickstart.",
+    "description": "En esta entrada se indican algunos de los URL y/o sitios consultados para realizar este trabajo compartido.",
+    "tags": [
+      "Tutorial",
+      "Hugo"
+    ],
+    "title": "Jekyll - referencias consultadas",
+    "uri": "/jekyll-on-linux/jekyll-references/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
     "tags": [
       "Programación",
       "Generadores Sitios Estáticos",
@@ -50,11 +61,19 @@ var relearn_searchindex = [
       "Fedora",
       "Linux"
     ],
-    "title": "Jekyll - referencias consultadas",
-    "uri": "/jekyll-on-linux/jekyll-references/index.html"
+    "title": "Artículos sobre Jekyll",
+    "uri": "/index.html"
   },
   {
-    "breadcrumb": "Inicio \u003e Tags",
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "Tags",
     "content": "",
     "description": "",
     "tags": [],
@@ -62,7 +81,7 @@ var relearn_searchindex = [
     "uri": "/tags/fedora/index.html"
   },
   {
-    "breadcrumb": "Inicio \u003e Tags",
+    "breadcrumb": "Tags",
     "content": "",
     "description": "",
     "tags": [],
@@ -70,21 +89,23 @@ var relearn_searchindex = [
     "uri": "/tags/generadores-sitios-est%C3%A1ticos/index.html"
   },
   {
-    "breadcrumb": "",
-    "content": "Artículos tipo capítulos Jekyll sobre Linux",
-    "description": "Artículos tipo capítulos Jekyll sobre Linux",
-    "tags": [
-      "Programación",
-      "Generadores Sitios Estáticos",
-      "Jekyll",
-      "Fedora",
-      "Linux"
-    ],
-    "title": "Inicio",
-    "uri": "/index.html"
+    "breadcrumb": "Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Hugo",
+    "uri": "/tags/hugo/index.html"
   },
   {
-    "breadcrumb": "Inicio \u003e Tags",
+    "breadcrumb": "Categories",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Category :: Hugo",
+    "uri": "/categories/hugo/index.html"
+  },
+  {
+    "breadcrumb": "Tags",
     "content": "",
     "description": "",
     "tags": [],
@@ -92,7 +113,7 @@ var relearn_searchindex = [
     "uri": "/tags/jekyll/index.html"
   },
   {
-    "breadcrumb": "Inicio \u003e Tags",
+    "breadcrumb": "Tags",
     "content": "",
     "description": "",
     "tags": [],
@@ -100,7 +121,7 @@ var relearn_searchindex = [
     "uri": "/tags/linux/index.html"
   },
   {
-    "breadcrumb": "Inicio \u003e Tags",
+    "breadcrumb": "Tags",
     "content": "",
     "description": "",
     "tags": [],
@@ -108,7 +129,7 @@ var relearn_searchindex = [
     "uri": "/tags/programaci%C3%B3n/index.html"
   },
   {
-    "breadcrumb": "Inicio",
+    "breadcrumb": "",
     "content": "",
     "description": "",
     "tags": [],
@@ -116,11 +137,35 @@ var relearn_searchindex = [
     "uri": "/tags/index.html"
   },
   {
-    "breadcrumb": "Inicio",
+    "breadcrumb": "Tags",
     "content": "",
     "description": "",
     "tags": [],
-    "title": "Categories",
-    "uri": "/categories/index.html"
+    "title": "Tag :: Tutorial",
+    "uri": "/tags/tutorial/index.html"
+  },
+  {
+    "breadcrumb": "Categories",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Category :: Tutorial",
+    "uri": "/categories/tutorial/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "frontmatter",
+    "uri": "/frontmatter/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "options",
+    "uri": "/options/index.html"
   }
 ]
