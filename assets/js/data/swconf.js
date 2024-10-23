03cec2fe4104f37229d9fa4ebf5f9dadee30e15e
Daniel Oscar Zamo <daniel.oscar.zamo@gmail.com>
2024-10-23 04:16:02 +0200

    commit Wed Oct 23 04:16:02 AM CEST 2024

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1729649378',resources: [
+    cacheName: 'chirpy-1729649762',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
