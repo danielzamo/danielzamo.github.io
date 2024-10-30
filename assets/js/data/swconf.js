0dcc0932f5bc5bf293634170696ae9e72358d263
Daniel Oscar Zamo <daniel.oscar.zamo@gmail.com>
2024-10-30 01:55:38 +0100

    site Jekyll - theme chirpy ver 1

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1730167486',resources: [
+    cacheName: 'chirpy-1730249663',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
